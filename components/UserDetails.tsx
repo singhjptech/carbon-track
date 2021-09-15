50d95df8ab71554d8daace3b67b5717fee477162
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-15 11:45:11 +0100

    add useEffect for vehicle reg api call

--- components/UserDetails.tsx
+++ components/UserDetails.tsx
@@ -1,39 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, StyleSheet, Text, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
+import { getData } from "../dbfunctions/api-functions.js";
 
 export type Props = {
   name?: string;
-  vehicleMake?: string;
-  vehicleModel?: any;
 };
 
-const UserDetails: React.FC<Props> = ({
-  vehicleMake,
-  vehicleModel,
-  navigation,
-}) => {
-  const [inputMake, setInputMake] = useState("");
-  const [inputModel, setInputModel] = useState("");
+const UserDetails: React.FC<Props> = ({ navigation }) => {
+  const [inputReg, setInputReg] = useState("");
+  const [submitReg, setSubmitReg] = useState("");
+  const [userVehicle, setUserVehicle] = useState({});
+  const [hasErrored, setHasErrored] = useState(false);
 
-  const handleSubmit = () => {};
+  useEffect(() => {
+    getData(submitReg)
+      .then((vehicle) => {
+        setUserVehicle(vehicle);
+      })
+      .catch((err) => {
+        if (err) {
+          setHasErrored(true);
+          setUserVehicle("");
+        }
+      });
+  }, [submitReg]);
+
+  const handleSubmit = () => {
+    setHasErrored(false);
+    setSubmitReg(inputReg);
+    setInputReg("");
+  };
+  console.log(submitReg, "<--Submitted");
+  console.log(userVehicle, "Veh Obj");
 
   return (
     <View style={styles.container}>
-      <Text style={styles.make}>Vehicle Make:</Text>
+      <Text style={styles.vehicleReg}>Enter Vehicle Registration:</Text>
       <TextInput
-        defaultValue={inputMake}
+        autoCapitalize="characters"
+        defaultValue={inputReg}
         placeholder="enter make"
         style={styles.input}
-        onChangeText={(inputMake) => setInputMake(inputMake)}
+        onChangeText={(inputReg) => setInputReg(inputReg)}
       />
-      <Text style={styles.model}>Vehicle Model:</Text>
-      <TextInput
-        defaultValue={inputModel}
-        placeholder="enter model"
-        style={styles.input}
-        onChangeText={(inputModel) => setInputModel(inputModel)}
-      />
+      {hasErrored ? (
+        <Text style={styles.inputError}>Error, invalid input</Text>
+      ) : null}
       <Button title="Submit" color="black" onPress={handleSubmit} />
       <Button
         title="Back"
@@ -55,14 +68,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  make: {
+  vehicleReg: {
     fontSize: 20,
     fontWeight: "bold",
     margin: 16,
   },
-  model: {
-    fontSize: 20,
-    fontWeight: "bold",
+  inputError: {
+    fontSize: 16,
+    color: "red",
     margin: 16,
   },
   input: {
