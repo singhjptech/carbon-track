79e5696d8a0163f4dce22b4dff883748ce32f3ba
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-15 09:13:52 +0100

    add render of UserActivity

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import Header from "./Header";
+import UserActivity from "./UserActivity";
 
 export type Props = {
-  navigation?: string
+  navigation?: string;
 };
 
-const HomeScreen: React.FC<Props> = ({
-  navigation,
-}) => {
- 
-
+const HomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Header />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header navigation={navigation} />
+      <UserActivity />
+    </SafeAreaView>
   );
 };
 
@@ -22,8 +21,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
-  }
+    justifyContent: "center",
+    backgroundColor: "#2F4847",
+    margin: 0,
+  },
 });
 
 export default HomeScreen;
