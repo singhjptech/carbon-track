ba80e0d92489cefd7739176e230aafa3cc2ed689
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-15 11:45:31 +0100

    refactor request

--- dbfunctions/api-functions.js
+++ dbfunctions/api-functions.js
@@ -1,48 +1,37 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const data = JSON.stringify({ registrationNumber: 'AA19AAA' });
+// const data = JSON.stringify({ registrationNumber: "AA19AAA" });
 
-const config = {
-  method: 'post',
-  url:
-    'https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles',
-  headers: {
-    'x-api-key': 'zsisXTnLTG7Ho55QW6rYY2A9oYp5LldN9eOLjt2i',
-    'Content-Type': 'application/json',
-  },
-  data: data,
-};
-
-// const vehicleData = {
-//   emissions: 0,
-//   make: '',
-//   fuelType: ''
-// }
-
-// const getData = async () => {
-//   await axios(config)
-//     .then((response) => {
-//       let vehicleData = {};
-//       vehicleData.emissions = response.data.co2Emissions;
-//       vehicleData.make = response.data.make;
-//       vehicleData.fuelType = response.data.fuelType;
-//       return vehicleData;
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//     })
+// const config = {
+//   method: "post",
+//   url: "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles",
+//   headers: {
+//     "x-api-key": "zsisXTnLTG7Ho55QW6rYY2A9oYp5LldN9eOLjt2i",
+//     "Content-Type": "application/json",
+//   },
+//   data: reg,
 // };
 
-const getData = async () => {
-  const { data } = await axios(config)
+const getData = async (regNum) => {
+  console.log(regNum, "<-- regNum in GET DATA");
+  const reg = JSON.stringify({ registrationNumber: regNum });
+  const config = {
+    method: "post",
+    url: "https://driver-vehicle-licensing.api.gov.uk/vehicle-enquiry/v1/vehicles",
+    headers: {
+      "x-api-key": "zsisXTnLTG7Ho55QW6rYY2A9oYp5LldN9eOLjt2i",
+      "Content-Type": "application/json",
+    },
+    data: reg,
+  };
+  const { data } = await axios(config);
   let vehicleData = {};
   vehicleData.emissions = data.co2Emissions;
   vehicleData.make = data.make;
+  vehicleData.colour = data.colour;
+  vehicleData.year = data.yearOfManufacture;
   vehicleData.fuelType = data.fuelType;
-  console.log(vehicleData, '<- OUR DATA')
   return vehicleData;
-}
-
-console.log(getData());
+};
 
 module.exports = { getData };
