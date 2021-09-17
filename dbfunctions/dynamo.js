7cc15db6f229dc2dc807af865897f24884d56d59
Bora Kim <stellakkkk@hotmail.com>
2021-09-17 10:57:23 +0100

    Implement getCar, getUser, AddGroup, joinGroup function

--- dbfunctions/dynamo.js
+++ dbfunctions/dynamo.js
@@ -12,7 +12,10 @@ const getTable = async () => {
     });
 }
 const addCar = async (car) => {
-    await dynamodb.put({
+    const userData = await dynamodb.get({TableName: 'UserData', Key:{UserName: Auth.user.username} }).promise();
+    
+    if (Object.keys(userData).length === 0) {
+        await dynamodb.put({
         TableName: 'UserData',
         Item: {
             UserName: Auth.user.username,
@@ -24,9 +27,31 @@ const addCar = async (car) => {
             }]
         }
     }).promise();
+    } else {
+        userData.Vehicles.push({
+            emissions: car.emissions,
+            make: car.make,
+            year: car.year,
+            fuelType: car.fuelType
+        })
+        await dynamodb.put({
+            TableName: 'UserData',
+            Item: userData
+    }).promise();
+    }
 };
 
 const getCar = async () => {
+    const vehicles = await dynamodb.get({
+        TableName: 'UserData',
+        Key: {
+            UserName: Auth.user.username
+        }
+    }).promise();
+    return vehicles.Vehicles;
+};
+
+const getUser = async () => {
     const userAndVehicles = await dynamodb.get({
         TableName: 'UserData',
         Key: {
@@ -36,4 +61,37 @@ const getCar = async () => {
     return userAndVehicles;
 };
 
-export { getTable, addCar };
+const addGroup = async (groupData) => {
+    try {
+        await dynamodb.put({ 
+            TableName: 'GroupData',
+            Item: {
+                GroupCode: groupData.groupCode,
+                GroupName: groupData.groupName, 
+                GroupCreator: Auth.user.username, 
+                GroupMembers: groupData.groupMembers }
+            }).promise();
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+const joinGroup = async (groupData) => {
+    const group = await dynamodb.get({TableName:'GroupData', Key: {groupName: groupData.groupName}}).promise();
+
+    if(Object.keys(group).length === 0) {
+        //do nothing
+    } else {
+        if(group.groupCode === groupData.groupCode) {
+            group.groupMembers.push(Auth.user.username);
+            await dynamodb.put({TableName:'GroupData', Item:{groupName:group.groupName, groupMembers: group.groupMembers}}).promise();
+        }
+    }
+};
+
+const getGroup = async (groupName) => {
+    return await dynamodb.get({TableName:'GroupData', Key: {groupName: groupName}}).promise();
+
+}
+
+export { getTable, addCar, addGroup, joinGroup, getGroup, dynamodb }
