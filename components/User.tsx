fdfaa5689e063e68aed821fb60903102a858b577
Matt_B <77924798+mattbushdev@users.noreply.github.com>
2021-09-20 22:01:08 +0100

    amend styling and buttons

--- components/User.tsx
+++ components/User.tsx
@@ -1,9 +1,6 @@
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
-import {
-  SafeAreaView,
-  withSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import GroupStats from "./GroupStats";
 import UserStats from "./UserStats";
 
@@ -14,18 +11,23 @@ export type Props = {
 const User: React.FC<Props> = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.buttonHome}>
-        <Button
-          title="Home"
-          color="white"
+      <View style={styles.buttonHomeContainer}>
+        <Pressable
+          style={styles.buttonHome}
           onPress={() => navigation.navigate("Home")}
-        />
+        >
+          <Text style={styles.buttonHomeText}>Home</Text>
+        </Pressable>
       </View>
       <View style={styles.userHeader}>
         <Text style={styles.userWelcome}>Hey, User</Text>
       </View>
-      <UserStats navigation={navigation} />
-      <GroupStats navigation={navigation} />
+      <View style={styles.statsContainer}>
+        <UserStats navigation={navigation} />
+      </View>
+      <View style={styles.statsContainer}>
+        <GroupStats navigation={navigation} />
+      </View>
     </SafeAreaView>
   );
 };
@@ -33,27 +35,57 @@ const User: React.FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    backgroundColor: "#2F4847",
+    backgroundColor: "white",
+    borderWidth: 4,
+    borderColor: "red",
   },
   headerText: {
     alignItems: "center",
     justifyContent: "center",
   },
   userHeader: {
+    color: "black",
     alignItems: "center",
     justifyContent: "center",
+    borderWidth: 2,
+    borderColor: "purple",
   },
   userWelcome: {
-    color: "white",
-    fontSize: 32,
+    fontSize: 28,
+    fontWeight: "bold",
   },
-  buttonHome: {
+  buttonHomeContainer: {
     alignItems: "flex-end",
     justifyContent: "flex-start",
+    marginRight: 20,
+    borderWidth: 2,
+    borderColor: "blue",
+  },
+  statsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "90%",
+    height: 250,
+    borderRadius: 28,
+    backgroundColor: "#D7E7E1",
+    margin: 20,
+    borderWidth: 2,
+    borderColor: "green",
+  },
+  buttonHome: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderColor: "#2F4847",
+    backgroundColor: "#2F4847",
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 5,
+    width: 80,
   },
   buttonHomeText: {
-    fontSize: 40,
+    color: "white",
+    fontSize: 20,
   },
 });
 
